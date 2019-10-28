323646e7c7289091b8d1f3fa1187c9f0c79b317c
Emily Platzer <emilyplatzer@gmail.com>
2019-10-28 15:57:29 -0600

    User can load multiple signup sheets for lasers and get them concatonated properly

--- spec/signupSheetCompiler.spec.js
+++ spec/signupSheetCompiler.spec.js
@@ -14,4 +14,20 @@ describe('Signup Sheet Compiler', () => {
 
     await fs.unlinkSync('./spec/fixtures/signups/GALasersOneTime-compiled.csv')
   })
+
+  it('generates a Laser Guided Access sheet from two signup sheets', async () => {
+    const filePaths = [
+      './spec/fixtures/signups/GALasersOneTime.csv',
+      './spec/fixtures/signups/GALasersTwoTime.csv',
+    ]
+
+    const compiledSheet = await signupSheetCompiler(filePaths, 'GALasersTwoTime')
+    const savedSheet = await fs.readFileSync('./spec/fixtures/signups/GALasersTwoTime-compiled.csv', 'utf8')
+    const expected = await fs.readFileSync('./spec/fixtures/signups/GALasersTwoTime-expected.csv', 'utf8')
+
+    expect(compiledSheet).toBe(expected)
+    expect(savedSheet).toBe(expected)
+
+    await fs.unlinkSync('./spec/fixtures/signups/GALasersTwoTime-compiled.csv')
+  })
 })
