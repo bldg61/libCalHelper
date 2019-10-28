f132505228cd45684826cf932c74e9633a344ceb
Emily Platzer <emilyplatzer@gmail.com>
2019-10-28 15:29:45 -0600

    Laser signups can be printed for one sesssion

---
+++ spec/signupSheetCompiler.spec.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const signupSheetCompiler = require('../lib/signupSheetCompiler');
+
+describe('Signup Sheet Compiler', () => {
+  it('generates a Laser Guided Access sheet from one signup sheet', async () => {
+    const filePath = './spec/fixtures/signups/GALasersOneTime.csv'
+
+    const compiledSheet = await signupSheetCompiler([filePath], 'GALasersOneTime')
+    const savedSheet = await fs.readFileSync('./spec/fixtures/signups/GALasersOneTime-compiled.csv', 'utf8')
+    const expected = await fs.readFileSync('./spec/fixtures/signups/GALasersOneTime-expected.csv', 'utf8')
+
+    expect(compiledSheet).toBe(expected)
+    expect(savedSheet).toBe(expected)
+
+    await fs.unlinkSync('./spec/fixtures/signups/GALasersOneTime-compiled.csv')
+  })
+})
