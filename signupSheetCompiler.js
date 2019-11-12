a64eecd9a3fec5b1b6f5048d27fa64d1bb54d134
Emily Platzer <emilyplatzer@gmail.com>
2019-11-12 16:54:56 -0700

    Added alert that the signup sheet compiler here is deprecated

--- signupSheetCompiler.js
+++ signupSheetCompiler.js
@@ -5,6 +5,11 @@ const signupSheetCompiler = require('./lib/signupSheetCompiler');
 const directory = './secretCSV/signupSheets/'
 const nameOfOutputFile = "Today's signups"
 
+console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+console.log("HAY FRIEND, WE ARE USING A WEB APP FOR THIS NOW:");
+console.log("http://bldg61cal.herokuapp.com/signupCompiler\n");
+console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
 fs.readdir(directory, function(err, directoryFiles) {
   signupSheetCompiler((
     directoryFiles.filter(file => {
